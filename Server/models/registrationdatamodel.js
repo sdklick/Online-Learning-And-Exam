7b76c679de6019109e52005ac674262408d91768
sdklick <sumantad827@gmail.com>
2024-03-20 01:56:35 +0530

    Change Model data Type

--- Server/models/registrationdatamodel.js
+++ Server/models/registrationdatamodel.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 let registrationschema = new mongoose.Schema({
   fullname: { type: String },
-  phoneno: { type: Number },
+  phoneno: { type: String },
   gender: { type: String },
   email: { type: String },
-  age: { type: Number },
+  age: { type: String },
   checkexamkey: { type: String },
   marks: { type: Object },
+  findsubject_examcode: { type: Object },
 });
 
 const registrationmodel = mongoose.model(
