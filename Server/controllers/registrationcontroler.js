c34f9796389ddf80d640c1ceda5db53bdb7d80e7
sdklick <sumantad827@gmail.com>
2024-03-20 02:03:18 +0530

    add Subject Field

--- Server/controllers/registrationcontroler.js
+++ Server/controllers/registrationcontroler.js
@@ -9,11 +9,20 @@ const handelregisdata = async (req, res) => {
   let checkemailandexamkey = await registrationmodel.findOne({
     $and: [{ checkexamkey: data.checkexamkey }, { email: data.email }],
   });
+
+  let findsubject_examcode = await questionsetschemamodel.findOne(
+    {
+      examkey: data.checkexamkey,
+    },
+    { examsubject: 1, _id: 0 }
+  );
+
   if (response == null) {
     res.send({ found: false });
   } else {
     if (checkemailandexamkey == null) {
-      await registrationmodel.insertMany(data);
+      let add = { ...data, findsubject_examcode };
+      await registrationmodel.insertMany(add);
       res.send({ found: true, rdata: response });
     } else {
       res.send({ found: "examsubmit" });
@@ -21,4 +30,4 @@ const handelregisdata = async (req, res) => {
   }
 };
 
-module.exports={handelregisdata}
+module.exports = { handelregisdata };
