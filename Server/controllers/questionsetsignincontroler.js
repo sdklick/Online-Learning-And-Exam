ac65c348044a6cb610cc6442b01185a772ba968e
sdklick <sumantad827@gmail.com>
2024-04-08 11:53:12 +0530

    add jwt on setquestion

--- Server/controllers/questionsetsignincontroler.js
+++ Server/controllers/questionsetsignincontroler.js
@@ -1,15 +1,20 @@
 const { signupdatamodel } = require("../models/signupmodel");
+const jwt = require("jsonwebtoken");
+const jwtkey = process.env.JWTKEY;
 
 const qsetsignin = async (req, res) => {
-  let getdata = req.query.ID;
+  let getdata = req.body;
   let response = await signupdatamodel.findOne({
     user_name: getdata.user_name,
     password: getdata.password,
   });
+
   if (response == null) {
     res.send({ found: false });
   } else {
-    res.send({ found: true, username: response.person_name });
+    jwt.sign({ response }, jwtkey, { expiresIn: "2h" }, (err, token) => {
+      res.send({ found: true, username: response.person_name, auth: token });
+    });
   }
 };
 
